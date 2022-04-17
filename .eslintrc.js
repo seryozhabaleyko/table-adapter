86a7d583809fed67f1bef5cbe254206083ea6994
Sergey Baleyko <seryozha.baleyko@gmail.com>
2022-04-17 19:32:31 +0300

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'airbnb-typescript',
+        'airbnb/hooks',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.eslint.json'
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    plugins: [
+        'react',
+        '@typescript-eslint',
+    ],
+    rules: {
+        // https://eslint.org/docs/rules/indent
+        indent: ['error', 4, { SwitchCase: 1 }],
+
+        // https://eslint.org/docs/rules/default-case
+        'default-case': 'off',
+
+        'import/prefer-default-export': 'off',
+
+        // only .tsx files may have JSX
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+        'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+
+        // Enforce JSX indentation
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent.md
+        'react/jsx-indent': ['error', 4, { checkAttributes: true }],
+
+        // Validate props indentation in JSX
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent-props.md
+        'react/jsx-indent-props': ['error', 4],
+
+        // Enforce a specific function type for function components
+        // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/function-component-definition.md
+        'react/function-component-definition': ['error', {
+            namedComponents: 'arrow-function',
+            unnamedComponents: 'arrow-function',
+        }],
+    },
+};
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { TableProps } from './types';
+
+export const Table: React.FC<TableProps> = () => {
+    return (
+        <div>Table</div>
+    )
+}
